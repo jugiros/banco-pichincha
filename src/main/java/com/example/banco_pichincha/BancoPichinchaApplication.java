ca317e55008549fea0f16b21b03b3cedc3999a78
Juan Molina <jmolina@initiumsoft.com>
2024-06-22 18:15:38 -0500

    first commit

---
+++ src/main/java/com/example/banco_pichincha/BancoPichinchaApplication.java
@@ -0,0 +1,13 @@
+package com.example.banco_pichincha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoPichinchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoPichinchaApplication.class, args);
+	}
+
+}
